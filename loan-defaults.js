29c47a0452eadfb59640adbba464caf0f6f0eed9
fradec <adecastelbajac@gmx.com>
2026-09-04 17:02:06 +0200

    chore: remove obsolete loan code

--- loan-defaults.js
+++ loan-defaults.js
@@ -1,12 +1,3 @@
-export const DEFAULT_VALUES = {
-  price: 300000,
-  downPayment: 30000,
-  isNew: 'ancien',
-  works: 0,
-  rate: 4.2,
-  duration: 20,
-};
-
 export const NOTARY_FEE_RATE = {
   neuf: 0.03,
   ancien: 0.08,
